a1be86434185b92d1450a12bb9b8ee473fd5ae7b
KT147 <kevin.tammm@gmail.com>
2026-05-13 15:41:54 +0300

    Map

---
+++ src/setsAndMaps/MapViewsMain.java
@@ -0,0 +1,73 @@
+package setsAndMaps;
+
+import java.util.*;
+
+public class MapViewsMain {
+
+    public static void main(String[] args) {
+
+        Map<String, Contact> contacts = new HashMap<>();
+        ContactData.getData("phone").forEach(c -> contacts.put(c.getName(), c));
+        ContactData.getData("email").forEach(c -> contacts.put(c.getName(), c));
+
+        Set<String> keysView = contacts.keySet();
+        System.out.println(keysView);
+
+        Set<String> copyOfKeys = new TreeSet<>(contacts.keySet());
+        System.out.println(copyOfKeys);
+
+        if (contacts.containsKey("Linus Van Pelt")) {
+            System.out.println("Exists");
+        }
+
+        keysView.remove("Daffy Duck");
+        System.out.println(keysView);
+        contacts.forEach((k, v) -> System.out.println(v));
+
+        copyOfKeys.remove("Linus Van Pelt");
+        System.out.println(copyOfKeys);
+        contacts.forEach((k, v) -> System.out.println(v));
+
+        keysView.retainAll(List.of("Linus Van Pelt"));
+        System.out.println(keysView);
+        contacts.forEach((k, v) -> System.out.println(v));
+
+        keysView.clear();
+        System.out.println(contacts);
+        ContactData.getData("email").forEach(c -> contacts.put(c.getName(), c));
+        ContactData.getData("phone").forEach(c -> contacts.put(c.getName(), c));
+        System.out.println(keysView);
+
+        var values = contacts.values();
+        values.forEach(System.out::println);
+
+        values.retainAll(ContactData.getData("email"));
+        System.out.println(keysView);
+        contacts.forEach((k,v) -> System.out.println(v));
+
+        System.out.println("-------------");
+        List<Contact> list = new ArrayList<>(values);
+        list.sort(Comparator.comparing(Contact::getNameLastFirst));
+        list.forEach(c -> System.out.println(c.getNameLastFirst() + ": " + c));
+
+        System.out.println("-------------");
+        Contact first = list.get(0);
+        contacts.put(first.getNameLastFirst(), first);
+        values.forEach(System.out::println);
+        keysView.forEach(System.out::println);
+
+        HashSet<Contact> set = new HashSet<>(values);
+        set.forEach(System.out::println);
+        if (set.size() < contacts.keySet().size()) {
+            System.out.println("Duplicates");
+        }
+
+        var nodeSet = contacts.entrySet();
+        for (var node : nodeSet) {
+            if (!node.getKey().equals(node.getValue().getName())) {
+                System.out.println("Does not match");
+                node.getValue();
+            }
+        }
+    }
+}
